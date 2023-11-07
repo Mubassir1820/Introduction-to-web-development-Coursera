034690d84ec537ee9b3044e5c632277769100cb8
Mubassir Habib <66573095+Mubassir1820@users.noreply.github.com>
2023-11-07 23:39:11 +0600

    Adding onblur event for last name validation

--- formValidation.js
+++ formValidation.js
@@ -2,7 +2,15 @@ function validateFirstName() {
 	var Fname = document.getElementById('fname').value;
 	
 	if(Fname==''){
-		alert('name must be filled out');
+		alert('Name must be filled out');
+		return false;
+	}
+}
+function validateLastName() {
+	var Lname = document.getElementById('lname').value;
+	
+	if(Lname=='') {
+		document.getElementById('msg2').innerHTML = 'Please fill out the last name';
 		return false;
 	}
 }
